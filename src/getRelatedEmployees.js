ac11ededb0f7bd5bcbc06532e1fcead092405f89
Alan Foster <alan-af@outlook.com>
2022-07-28 15:33:43 -0300

    4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,20 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return data.employees
+    .some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const colabor = data.employees
+    .filter((managerCollaborator) => managerCollaborator.managers.includes(managerId));
+  const result = colabor
+    .map((collaborator) => `${collaborator.firstName} ${collaborator.lastName}`);
+  return result;
 }
 
 module.exports = { isManager, getRelatedEmployees };
