25bde52755d11397801674d5126a7512180dc6e7
Alan Foster <alan-af@outlook.com>
2022-07-28 16:53:01 -0300

    8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,28 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const soloChild = entrants.filter((child) => child.age < 18).length;
+  const soloAdult = entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length;
+  const soloSenior = entrants.filter((senior) => senior.age >= 50).length;
+  return {
+    child: soloChild,
+    adult: soloAdult,
+    senior: soloSenior,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const price = data.prices;
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const solotChild = entrants.filter((child) => child.age < 18).length;
+  const solotAdult = entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length;
+  const soloSenior = entrants.filter((senior) => senior.age >= 50).length;
+  const total = (solotChild * price.child)
+    + (solotAdult * price.adult)
+    + (soloSenior * price.senior);
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
