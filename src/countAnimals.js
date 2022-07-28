f06e23a83958ece1cb57d9369e8f321ab7a48413
Alan Foster <alan-af@outlook.com>
2022-07-28 15:43:50 -0300

    5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,17 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const arraySpecies = data.species;
+  if (!animal) {
+    return Object.fromEntries(arraySpecies
+      .map((element) =>
+        [element.name, element.residents.length]));
+  }
+  const specieSelect = arraySpecies.find((element) => element.name === animal.specie).residents;
+  if (animal.sex) {
+    return specieSelect.filter((specie) => specie.sex === animal.sex).length;
+  }
+  return specieSelect.length;
 }
 
 module.exports = countAnimals;
