5de7481c8b071fd7cf8f1b32271453c7939742bc
Alan Foster <alan-af@outlook.com>
2022-07-28 14:58:24 -0300

    1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aquia
+function getSpeciesByIds(...ids) {
+  // seu código aqui
+  const selectId = ids.map((id) => data.species.find((specie) => specie.id === id));
+  return !ids ? [] : selectId;
 }
 
 module.exports = getSpeciesByIds;
