356d14278a7d987ac07d80512e4a84184dde68cb
Alan Foster <alan-af@outlook.com>
2022-07-28 18:58:07 -0300

    11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const result = data.employees.map((elementSpecie) => (
+  {
+    id: elementSpecie.id,
+    fullName: `${elementSpecie.firstName} ${elementSpecie.lastName}`,
+    species: elementSpecie.responsibleFor.map((elementId) => (
+      data.species.find((element1) => (element1.id === elementId)).name
+    )),
+    locations: elementSpecie.responsibleFor.map((elementId) => (
+      data.species.find((element2) => (element2.id === elementId)).location
+    )),
+  }
+));
+
+function getEmployeesCoverage(nameOrId) {
+  const condit = nameOrId && result.find((element) => element.id === nameOrId.id
+  || element.fullName.split(' ')[0] === nameOrId.name
+  || element.fullName.split(' ')[1] === nameOrId.name);
+  if (condit) {
+    return condit;
+  }
+  if (nameOrId) {
+    throw new Error('Informações inválidas');
+  }
+  return result;
 }
 
 module.exports = getEmployeesCoverage;
